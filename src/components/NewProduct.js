f31bd4563a09ea628ec1da0c46dc075c52c10ed7
elizabeth <ellizzabbetth@gmail.com>
2019-06-09 08:59:53 -0400

    customize photoPicker component functionality and style

--- src/components/NewProduct.js
+++ src/components/NewProduct.js
@@ -3,19 +3,34 @@ import { PhotoPicker } from 'aws-amplify-react';
 //prettier-ignore
 import { Form, Button, Input, Notification, Radio, Progress } from "element-react";
 
+
+const initialState= {
+  description: "",
+  price: "",
+  shipped: false,
+  imagePreview: "",
+  image: ""
+};
+
 class NewProduct extends React.Component {
+  // To clear out state from form when it's submitted :
+  // 1. swap out our state object with all the initial state values
+  // 2. move state obj outside component and create it as a variable called initial state
+  // 3. within state obj spread all the individual properties and values of initial state
+  // 4. In handleAddProduct after adding product call setState and spread in initial state 
+  // in there as well.
   state = {
-    description: "",
-    price: "",
-    shipped: false
+    ...initialState
   };
 
   handleAddProduct = () => {
-    console.log("product added");
+    console.log("add product", this.state);
+    // this clears out al the values that we stored in state after we logged state
+    this.setState({ ...initialState})
   };
 
   render() {
-    const { shipped } = this.state;
+    const { description, price, image, shipped, imagePreview } = this.state;
     
     return (
       <div className="flex-center">
@@ -28,7 +43,9 @@ class NewProduct extends React.Component {
                 type="text"
                 icon="information"
                 placeholder="Description"
-                onChange={price => this.setState({ price })}
+                /* make controlled input by passing value */
+                value={description}
+                onChange={description => this.setState({ description })}
 
               />
             </Form.Item>
@@ -38,7 +55,9 @@ class NewProduct extends React.Component {
                 type="number"
                 icon="plus"
                 placeholder="Price ($USD)"
-                onChange={description => this.setState({ description })}                
+                /* make controlled input by passing value */
+                value={price}
+                onChange={price => this.setState({ price })}                
               />
             </Form.Item>
             <Form.Item label="Is the Product Shipped or Emailed to the Customer?">
@@ -59,9 +78,50 @@ class NewProduct extends React.Component {
                  
 
             </Form.Item>
-            <PhotoPicker />
+              {imagePreview && (
+                <img 
+                className="image-preview"
+                src={imagePreview}
+                alt= "Product Preview"
+                />
+
+              )}
+              <PhotoPicker 
+                title="Product Image"
+                preview= "hidden"
+                onLoad={url => this.setState({ imagePreview: url })}
+                /* load image file and put it in state so that we 
+                can send it off with our create product mutation when we 
+                execute it  */
+              
+                onPick={file => this.setState({ image: file })}
+                theme={{
+                  formContainer: { marge: 0,
+                  padding: '0.8em'
+                  },
+                  formSection: {
+                    display: 'flex',
+                    alignItems: 'center'
+
+                  },
+                  sectionBody: {
+                    margin: 0,
+                    width: "250px"
+                  },
+                  sectionHeader: {
+                    padding: "0.2em",
+                    color: "var(--darkAmazonOrange"
+                  },
+                  /* case sensitive */
+                  photoPickerButton: {
+                    display: 'none'
+                  }
+                
+                }}
+              />
             <Form.Item>
               <Button
+                disabled={!image || !description || !price}
                 type="primary"
                 onClick={this.handleAddProduct}
               >
